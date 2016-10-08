f30371e76c9e1d212c8292898e98827166cf3382
Florent Flament <contact@florentflament.com>
2016-10-09 01:20:51 +0200

    Added threaded version of stream-sndh.py

--- stream-sndh.py
+++ stream-sndh.py
@@ -1,34 +1,56 @@
+import Queue
 import serial
 import sys
+import threading
 import time
 
-data = []
-for l in sys.stdin.readlines():
-    sample = []
-    _, ts, regs = l.split()
-    sample.append(chr(int(ts[-4:-2], 16)))
-    sample.append(chr(int(ts[-2:], 16)))
+class UART_manager(threading.Thread):
 
-    en_regs = filter(lambda x:x[1] != '..', enumerate(regs.split('-')))
-    sample.append(chr(len(en_regs)))
-    for n,r in en_regs:
-        sample.append(chr(n))
-        sample.append(chr(int(r,16)))
+    def __init__(self, fd, q):
+        super(UART_manager, self).__init__()
+        self.__fd = fd
+        self.__q = q
 
-    data.append(''.join(sample))
-s = ''.join(data)
+    def run(self):
+        time.sleep(1) # Wait for buffer to fill up
+        while not self.__q.empty():
+            # Read number of bytes to send
+            available = ord(self.__fd.read())
+            available = (available<<8) + ord(self.__fd.read())
+            sys.stdout.write("\x1b[2K\rYM_empty: {}\tPC_buffer: {}".format(available, self.__q.qsize()))
+            sys.stdout.flush()
+            chunk_size = min(available, self.__q.qsize())
+            self.__fd.write(chr(chunk_size>>8 & 0xff) + chr(chunk_size & 0xff))
+            self.__fd.read() # Ack
+            self.__fd.write(''.join([self.__q.get() for _ in xrange(chunk_size)]))
 
-print "*****", len(s)
-fd = serial.Serial("/dev/ttyUSB0", 1000000, stopbits=1, timeout=1)
-time.sleep(2) # Waiting for arduino initialization
-i = 0
-while i < len(s):
-    cnt = ord(fd.read())
-    #cnt = cnt if i+cnt < len(s) else len(s)-i
-    j = i+cnt
-    print cnt, i, j
-    chunk = s[i:j]
-    #print [ord(c) for c in chunk]
-    fd.write(chr(cnt) + chunk)
-    i = j
-fd.close()
+
+def main():
+    fd = serial.Serial("/dev/ttyUSB0", 1000000, timeout=2)
+    q = Queue.Queue()
+    um = UART_manager(fd, q)
+
+    um.start()
+    l = sys.stdin.readline()
+    while l:
+        if q.qsize() < 3000:
+            _, ts, regs = l.split()
+            q.put(chr(int(ts[-4:-2], 16)))
+            q.put(chr(int(ts[-2:], 16)))
+            en_regs = filter(lambda x:x[1] != '..', enumerate(regs.split('-')))
+            for n,r in en_regs:
+                q.put(chr(n))
+                q.put(chr(int(r,16)))
+            q.put('\xff')
+            l = sys.stdin.readline()
+        else:
+            time.sleep(0.01)
+    for c in '\x00\x00\x08\x00\x09\x00\x0a\x00\xff': # Turn off channels
+        q.put(c)
+
+    um.join()
+    fd.close()
+    print ""
+
+
+main()
