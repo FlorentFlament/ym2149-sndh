a02bc874ea6377ee46048fe664ba05b3ce8d6cc1
Florent Flament <contact@florentflament.com>
2019-06-24 01:47:34 +0200

    Move stream-ym.py to python3

--- stream-ym.py
+++ stream-ym.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import functools
 import itertools
@@ -7,13 +7,18 @@ import serial
 import sys
 import time
 
+DEBUG = False
+
 class YmReader(object):
 
     def __parse_extra_infos(self):
-        # Thanks http://stackoverflow.com/questions/32774910/clean-way-to-read-a-null-terminated-c-style-string-from-a-file
-        toeof = iter(functools.partial(self.__fd.read, 1), '')
         def readcstr():
-            return ''.join(itertools.takewhile('\0'.__ne__, toeof))
+            l = []
+            c = self.__fd.read(1)[0]
+            while c != 0:
+                l.append(c)
+                c = self.__fd.read(1)[0]
+            return bytes(l).decode('ascii')
         self.__header['song_name'] = readcstr()
         self.__header['author_name'] = readcstr()
         self.__header['song_comment'] = readcstr()
@@ -23,7 +28,8 @@ class YmReader(object):
         # http://leonard.oxg.free.fr/ymformat.html
         # ftp://ftp.modland.com/pub/documents/format_documentation/Atari%20ST%20Sound%20Chip%20Emulator%20YM1-6%20(.ay,%20.ym).txt
         ym_header = '> 4s 8s I I H I H I H'
-        s = self.__fd.read(struct.calcsize(ym_header))
+        sz = struct.calcsize(ym_header)
+        s = self.__fd.read(sz)
         d = {}
         (d['id'],
          d['check_string'],
@@ -45,8 +51,8 @@ class YmReader(object):
 
     def __read_data_interleaved(self):
         cnt  = self.__header['nb_frames']
-        regs = [self.__fd.read(cnt) for i in xrange(16)]
-        self.__data=[''.join(f) for f in zip(*regs)]
+        regs = [self.__fd.read(cnt) for i in range(16)]
+        self.__data = zip(*regs)
 
     def __read_data(self):
         if not self.__header['interleaved']:
@@ -55,8 +61,8 @@ class YmReader(object):
         self.__read_data_interleaved()
 
     def __check_eof(self):
-        if self.__fd.read(4) != 'End!':
-            print '*Warning* End! marker not found after frames'
+        if self.__fd.read(4) != b'End!':
+            print('*Warning* End! marker not found after frames')
 
     def __init__(self, fd):
         self.__fd = fd
@@ -67,7 +73,7 @@ class YmReader(object):
         for k in ('id','check_string', 'nb_frames', 'song_attributes',
                   'nb_digidrums', 'chip_clock', 'frames_rate', 'loop_frame',
                   'extra_data', 'song_name', 'author_name', 'song_comment'):
-            print "{}: {}".format(k, self.__header[k])
+            print("{}: {}".format(k, self.__header[k]))
 
     def get_header(self):
         return self.__header
@@ -85,41 +91,72 @@ def to_minsec(frames, frames_rate):
     secs = secs % 60
     return (mins, secs)
 
+def dump_ym(data):
+    for d in data:
+        print(" ".join([("${:02x}".format(v)) for v in d]))
+
+def dump_stream(stream):
+    ln = []
+    i = 0
+    for n in stream:
+        ln.append(n)
+        if i == 1:
+            ts = (ln[0]<<8) + ln[1]
+            print(str(ts).ljust(5), end="  ")
+        if i != 0 and i%2 == 0 and n == 0xff:
+            print(" ".join([("${:02x}".format(v)) for v in ln]))
+            ln = []
+            i = 0
+        else:
+            i += 1
+
 def main():
     header = None
     data = None
 
     if len(sys.argv) != 3:
-        print "Syntax is: {} <output_device> <ym_filepath>".format(sys.argv[0])
+        print("Syntax is: {} <output_device> <ym_filepath>".format(sys.argv[0]))
         exit(0)
 
-    with open(sys.argv[2]) as fd:
+    ym_fname = sys.argv[2]
+    if DEBUG:
+        print("\nStarting parsing of {}".format(ym_fname))
+
+    with open(ym_fname, 'rb') as fd:
         ym = YmReader(fd)
         ym.dump_header()
         header = ym.get_header()
-        data = ym.get_data()
-    data.append('\x00'*16) # Mute song - Doesn't work as a
+        data = list(ym.get_data()) # This returns the samples as integers
+
+    if DEBUG:
+        print("\nParsed {} successfully".format(ym_fname))
+        dump_ym(data)
+        print("\nBuilding board data stream")
 
     data_ts = []
     ts = 0
     for d in data:
-        data_ts.append(chr(ts>>8 & 0xff))
-        data_ts.append(chr(ts & 0xff))
+        data_ts.append(ts>>8 & 0xff)
+        data_ts.append(ts & 0xff)
         # r0-r12 are updated on YM all the time
         for i,v in enumerate(d[:13]):
-            data_ts.append(chr(i))
+            data_ts.append(i)
             data_ts.append(v)
         # r13 (Waveform shape) is only updated if != 0xff
-        if d[13] != '\xff':
-            data_ts.append(chr(13))
+        if d[13] != 0xff:
+            data_ts.append(13)
             data_ts.append(d[13])
         # Not implementing DD (digi-drums) and TS (Timer-Synth)
         # Using bytes 14 and 15 of YM stream
-        data_ts.append('\xff') # Stop character
+        data_ts.append(0xff) # Stop character
         ts = (ts + 40000) & 0xffff
 
-    s = ''.join(data_ts)
-    print "Data size: {}".format(len(s))
+    if DEBUG:
+        print("\nBuilt board data stream successfully")
+        dump_stream(data_ts)
+
+    s = bytes(data_ts)
+    print("Data size: {}".format(len(s)))
 
     fd = serial.Serial(sys.argv[1], 1000000, timeout=2)
     # !!! https://github.com/torvalds/linux/blob/c05c2ec96bb8b7310da1055c7b9d786a3ec6dc0c/drivers/usb/serial/ch341.c
@@ -131,12 +168,11 @@ def main():
 
     i = 0
     while i < len(s):
-        cnt = ord(fd.read()) << 8
-        cnt+= ord(fd.read())
+        cnt = fd.read(1)[0] << 8
+        cnt+= fd.read(1)[0]
         cnt = min(cnt, len(s)-i)
-        fd.write(chr(cnt >> 8))
-        fd.write(chr(cnt & 0xff))
-        fd.read() # ACK
+        fd.write([cnt >> 8, cnt & 0xff])
+        fd.read(1) # ACK
         fd.write(s[i: i+cnt])
         i += cnt
     fd.close()
